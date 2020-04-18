cdd2a3a6311835a569b11054eb18a1b8c14249ff
kyleman1224@gmail.com <kyleman1224@gmail.com>
2020-04-18 17:40:05 -0400

    V2

--- login-page/src/main/java/com/packagename/CCE/Application.java
+++ login-page/src/main/java/com/packagename/CCE/Application.java
@@ -3,9 +3,6 @@ package com.packagename.CCE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
